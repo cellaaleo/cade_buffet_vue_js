cad10ce69501e7096e6e279f69d0a7305dcd3332
Marcella Bortoletto Aleo <cella.aleo@gmail.com>
2024-05-13 15:49:43 +0100

    Início do projeto - visualizando lista de buffets

---
+++ main.js
@@ -0,0 +1,30 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            brandName: '',
+            city: '',
+            state: '',
+
+            venuesList: [],
+        }
+    },
+
+    methods:{
+        async getData(){
+            let response = await fetch('http://localhost:3000/api/v1/venues/');
+            let data = await response.json();
+
+            data.forEach(item => {
+                var venue = new Object();
+                venue.brandName = item.brand_name;
+                venue.city = item.city;
+                venue.state = item.state;
+
+                this.venuesList.push(venue);
+            })
+        }
+    },
+    
+})
+
+app.mount('#app');
