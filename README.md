e9fe6d8c156a66dcad6e92a30e90b6dda0f338e1
Marcella Bortoletto Aleo <cella.aleo@gmail.com>
2024-05-14 14:57:20 +0100

    é possível ver tela de detalhes de um buffet ao clicar em seu nome

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Projeto VueJS para consumir dados da API da aplicação rails Cadê Buffet
 Rodar o rails server da app Cadê Buffet (localhost:3000) e abrir a página desta aplicação VueJS com o Live Server.
 
 No campo "Digite o nome do buffet" é possível filtrar a lista de buffets pelo nome fantasia.
+
+Ao clicar no nome de um buffet, é possível ver mais detalhes sobre ele.
+Para voltar na página inical, clicar em "Cadê Buffet?"
--- main.js
+++ main.js
@@ -3,11 +3,13 @@ const app = Vue.createApp({
         return{
             searchText: '',
             venuesList: [],
+            show: false,
+            venue: {},
         }
     },
 
     async mounted(){
-        await this.getData();
+        this.listResult = await this.getData();
     },
 
     computed:{
@@ -29,12 +31,37 @@ const app = Vue.createApp({
 
             data.forEach(item => {
                 var venue = new Object();
+                venue.id = item.id;
+                venue.link = `http://localhost:3000/api/v1/venues/${venue.id}`
                 venue.brandName = item.brand_name;
                 venue.city = item.city;
                 venue.state = item.state;
 
                 this.venuesList.push(venue);
             })
+        },
+
+        async showVenue(id){
+            let response = await fetch(`http://localhost:3000/api/v1/venues/${id}`);
+            let data = await response.json();
+            console.log(data);
+            this.show = true;
+
+            this.venue.brandName = data.brand_name;
+            this.venue.description = data.description;
+            this.venue.address = data.address;
+            this.venue.district = data.district;
+            this.venue.city = data.city;
+            this.venue.state = data.state;
+            this.venue.zipCode = data.zip_code;
+            this.venue.paymentMethods = data.payment_methods;
+            this.venue.email = data.email;
+            this.venue.phone = data.phone_number;
+        },
+
+        refresh(){
+            this.show = false;
+            this.searchText = '';
         }
     },
     
